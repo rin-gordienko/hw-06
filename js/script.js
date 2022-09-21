d49412b6c450c3791cc2fa285d6a794c1dcf299e
Irina Hordiienko <rin.gordienko@gmail.com>
2022-09-22 00:00:08 +0300

    added second task

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@
 
 let userName = prompt("What is your name?");
 alert(userName);
+// 2. Создать 2 переменные.
+// Провести между ними следующие математические операции: сложение, вычитание, умножение, деление и возведение в степень.
+// Результат выполнения каждой операции вывести в alert
+
+let a = 2;
+let b = 19;
+
+let sum = a + b;
+alert(sum);
+
+let subtr = b - a;
+alert(subtr);
+
+let multiplication = a * b;
+alert(multiplication);
+
+let division = b / a;
+alert(division);
+
+let power = b ** a;
+alert(power);
+
+let reminder = b % a;
+alert(reminder);
+
