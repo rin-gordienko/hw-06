cd939797a0824ad8dbd9f70afd194c778559686d
Irina Hordiienko <rin.gordienko@gmail.com>
2022-09-21 23:59:28 +0300

    added script.js and first task

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// 1. C помощью prompt спросить как зовут пользователя.
+// Вывести имя пользователя с помощью alert.
+
+let userName = prompt("What is your name?");
+alert(userName);
